ca67340d6043ac4b78dfb35215adc898f5ab2790
developma <developer.daisuke.maeno@gmail.com>
2018-01-16 21:46:30 +0900

    Add logics for review day.

--- review-timing/src/App.js
+++ review-timing/src/App.js
@@ -11,7 +11,12 @@ class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      startDate: moment()
+      startDate: moment(),
+      first: '',
+      second: '',
+      thrid: '',
+      fourth: '',
+      fifth: ''
     }
   }
 
@@ -21,17 +26,62 @@ class App extends Component {
     })
   }
 
+  onClick (e) {
+    const tmpDate = this.state.startDate
+    this.setState({
+      first: moment(tmpDate).format('YYYY/MM/DD'),
+      second: moment(tmpDate).add('days', 1).format('YYYY/MM/DD'),
+      thrid: moment(tmpDate).add('days', 7).format('YYYY/MM/DD'),
+      fourth: moment(tmpDate).add('M', 1).format('YYYY/MM/DD'),
+      fifth: moment(tmpDate).add('M', 6).format('YYYY/MM/DD')
+    })
+  }
+
 
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <DatePicker selected={this.state.startDate} onChange={e => this.handleChange(e)} />
-        </p>
+        <p className="App-intro"></p>
+        <DatePicker 
+          selected={this.state.startDate} 
+          onChange={e => this.handleChange(e)}
+          dateFormat="YYYY/MM/DD" />
+        <button onClick={e => this.onClick(e)}>calcuate</button>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Timing of review</th>
+              <th>scheduled day of review</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1st</td>
+              <td>{this.state.first}</td>
+            </tr>
+            <tr>
+              <td>2nd</td>
+              <td>{this.state.second}</td>
+            </tr>
+            <tr>
+              <td>3rd</td>
+              <td>{this.state.thrid}</td>
+            </tr>            
+            <tr>
+              <td>4th</td>
+              <td>{this.state.fourth}</td>
+            </tr>
+            <tr>
+              <td>5th</td>
+              <td>{this.state.fifth}</td>
+            </tr>  
+          </tbody>
+        </table>
       </div>
     );
   }
