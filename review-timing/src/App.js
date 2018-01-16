e2c2a85f677efe68ec1c4ee21ab208a77c706160
developma <developer.daisuke.maeno@gmail.com>
2018-01-16 15:38:26 +0900

    Add code for DatePicker.

--- review-timing/src/App.js
+++ review-timing/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DatePicker from 'react-datepicker'
+import moment from 'moment'
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment()
+    }
+  }
+
+  handleChange (date) {
+    this.setState({
+      startDate: date
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -11,7 +30,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <DatePicker selected={this.state.startDate} onChange={e => this.handleChange(e)} />
         </p>
       </div>
     );
